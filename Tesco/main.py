21522ff72400d4d3ec552ee085276a2d24c690b1
Ákos <kote2ster@gmail.com>
2019-09-28 19:00:01 +0200

    Add Tesco solution

--- Tesco/main.py
+++ Tesco/main.py
@@ -1,12 +1,18 @@
-import hug
+import hug, json, requests
+from db import DB
+
+pythonsqlite = 'pythonsqlite.db'
+
+db = DB(pythonsqlite)
+db.create_table()
 
 @hug.get()
-def index():
+def groceries(query: hug.types.text, offset: hug.types.number, limit: hug.types.number, rating: hug.types.number):
     import http.client, urllib.request, urllib.parse, urllib.error, base64
 
     headers = {
         # Request headers
-        'Ocp-Apim-Subscription-Key': '{subscription key}',
+        'Ocp-Apim-Subscription-Key': 'c7748d0ad5b447feb47c4bff27aadac0',
     }
 
     params = urllib.parse.urlencode({})
@@ -15,10 +21,17 @@ def index():
 
     try:
         conn = http.client.HTTPSConnection('dev.tescolabs.com')
-        conn.request("GET", "/grocery/products/?query={query}&offset={offset}&limit={limit}&%s" % params, "{body}", headers)
+        conn.request("GET", "/grocery/products/?query=" + str(query) + "&offset=" + str(offset) + "&limit=" + str(limit) + "&%s" % params, "{body}", headers)
         response = conn.getresponse()
         data = response.read()
-        print(data)
+        f = open('out.txt', 'w')
+        data = json.loads(str(data.decode()))
+        f.write(str(data))
+        results = data['uk']['ghs']['products']['results']
+        for result in results:
+            result['id_prod'] = result['id']
+            result['description'] = ''
+            db.insert_house(result)
         return_data['why'] = data
         conn.close()
     except Exception as e:
